68e9688636b9ed563827fe7996237d04052fb5fe
Mehmet Köse <mehmet.py@gmail.com>
2016-11-23 15:21:35 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ setup(
     author='Mehmet Kose',
     author_email='mehmet@linux.com',
     description='Rethinkdb Mixin For Tornado Framework.',
-    keywords='tornado rethinkdb layer',
+    platforms=('Any'),
+    keywords='tornado rethinkdb layer'.split(),
     install_requires=[
-        'tornado',
-        'rethinkdb',
+        'tornado>=2.3.0',
+        'rethinkdb>=4.4.2',
     ],
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
     ],
