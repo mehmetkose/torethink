7539c77fccb87d36fdaa3d858511e9e38b7a4d7a
Mehmet Köse <mehmet.py@gmail.com>
2016-11-23 14:26:33 +0300

    add methods

--- tests.py
+++ tests.py
@@ -6,11 +6,12 @@ from torethink import Torethink
 
 async def test():
     db = await Torethink.init(host="127.0.0.1", db="authentication", port=28015)
-    hello = await db.hello()
-    print(hello)
+    list = await db.list("user")
+    print(list)
 
 async def main():
     tornado.ioloop.IOLoop.current().spawn_callback(test)
+
 
 if __name__ == "__main__":
     tornado.ioloop.IOLoop.current().run_sync(main)
