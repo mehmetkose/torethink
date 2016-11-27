1aab8dbae5290f30fe1ffa3ce79b6fdc6ff7a5cd
Mehmet Kose <mehmetkose122@gmail.com>
2016-11-27 22:51:51 +0300

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='torethink',
-    version='2.0.0',
+    version='2.1.0',
     packages=['torethink'],
     url='https://github.com/mehmetkose/torethink',
     license='MIT',
--- torethink/__init__.py
+++ torethink/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Mehmet Kose"
 __email__ = "mehmet@linux.com"
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 from .torethink import Torethink
 from .database import Database, Table, Row
